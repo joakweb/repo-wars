18f1f12108cfb785172268d3efece8385da38939
Jorge Rodrigues Carvalho <jorgecarvalho@lampp-it.com.br>
2023-04-04 16:46:49 -0300

    feat: initial title and button (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from "react"
+import React, {useState} from "react"
 import './app.css'
 
 const App = () => {
-  return <div>App</div>
+  const [showGame, setShowGame] = useState<boolean>(false)
+  const [repos, setRepos] = useState([])
+  return <main>
+    {!showGame && (
+      <>
+      <h1>Welcome to repo-wars</h1>
+      <h2>A developer-driven guessing game</h2>
+      {/* TODO: continue */}
+      {repos.length === 0 ? (<p>loading game...</p>) : (<button>Start</button>)}
+      </>
+    )}
+    {showGame && (
+      // TODO: create game component
+      <>
+      <h1>Game</h1>
+      </>
+    )}
+  </main>
 }
 
 export default App
